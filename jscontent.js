4b0143ccddf98c2e30f4472dd855ca44dcdc42f6
HenryBcit <hleung70@bcit.ca>
2025-11-14 12:07:31 -0800

    midterm v2

--- jscontent.js
+++ jscontent.js
@@ -5,53 +5,55 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-//create your sleepTime variable here.
+// Create your homeworkTime variable here.
 
 /**
- * AskSleepDuration()
+ * AskHomeworkDuration()
  * ----------------
- * Function for basic sleep tracking.
- * - Asks the user how long they slept.
+ * Function for basic homework tracking.
+ * - Asks the user how long they did homework.
  * - Ends the program if the user types "exit".
- * - Gives encouraging responses based on hours of sleep.
+ * - Gives encouraging responses based on hours of homework.
  */
-function AskSleepDuration() {
-  readline.question('?QUESTION? (type "exit" to exit): ', (input) => {
+function AskHomeworkDuration() {
+  readline.question('?Question? (type "exit" to exit): ', (input) => {
+      
     if (input.toLowerCase() === "exit") {
-      console.log("Thanks for taking care of your rest!");
+      console.log("Thanks for tracking your study time!");
       readline.close();
       return;
     }
 
     /*Write your code below here*/
-    
+
     /*
     if(logic){
       //modify the variable and do the console log here
     }
     */
-    
+
     /*Write your code above here*/
   });
 }
 
 /**
- * TrackSleepStatus()
+ * TrackHomeworkStatus()
  * ---------------------
- * Function for advanced sleep tracking (Level 3 – Math & Logic).
+ * Function for advanced homework tracking.
  * - Asks what kind of help the user needs.
  * - Check if the answer is "track"
- * - If it's "track", then check what the sleepTime is. Add a number to the rest points based on the sleep time (see README.md)
+ * - If it's "track", then check what the homeworkTime is. Add a number to the focusPoints based on the homework time.
  * - Check if the answer is "stats"
- * - If it's "stats", then give a different console log response based on the amount of sleep points
+ * - If it's "stats", then give a different console log response based on the amount of focusPoints
  */
-function TrackSleepStatus() {
-  readline.question('?QUESTION? (type "exit" to exit): ', (input) => {
+function TrackHomeworkStatus() {
+  readline.question('?Question? ("exit" to exit): ', (input) => {
+      
     if (input.toLowerCase() === "exit") {
-      console.log("Thanks for tracking your rest. Sweet dreams!");
-      readline.close();
-      return;
-    }
+        console.log("Thanks for tracking your study. Good luck!");
+        readline.close();
+        return;
+      }
 
     /*Write your code below here*/
     
@@ -72,14 +74,14 @@ function TrackSleepStatus() {
 /**
  * AskInitialQuestion()
  * ----------------
- * Starting function for the sleep tracker.
- * - Asks if the user got some rest.
+ * Starting function for the homework tracker.
+ * - Asks if the user did any homework.
  * - Ends if they type "exit".
  * - Check for "yes" or "no"
- * - Leads into AskSleepDuration() or CheckSleepProblems().
+ * - Leads into AskHomeworkDuration() or CheckHomeworkProblems().
  */
 function AskInitialQuestion() {
-  readline.question('?QUESTION? (type "exit" to exit): ', (input) => {
+  readline.question('?Question? (or type "exit" to exit): ', (input) => {
     if (input.toLowerCase() === "exit") {
       console.log("See you next time!");
       readline.close();
@@ -89,34 +91,32 @@ function AskInitialQuestion() {
     /*Write your code below here*/
 
     /*
-    if(logic){
-      //write your console.log here
-    }
-    */
+      if(logic){
+        //write your console.log here
+      }
+      */
 
     /*Write your code above here*/
-    
   });
 }
 
 // Start the program
 AskInitialQuestion();
 
-
-//BONUS BELOW, IGNORE BELOW UNLESS YOU ARE DOING THE BONUS
+// BONUS BELOW, IGNORE BELOW UNLESS YOU ARE DOING THE BONUS
 
 /**
- * CheckSleepProblems()
+ * CheckHomeworkProblems()
  * ---------------------
- * Function for advanced sleep tracking.
- * - Asks "what happened last night?" to get the user to respond.
- * - Based on the answer from the user, deduct restPoints as indicated in README.md
+ * Function for advanced homework tracking.
+ * - Asks "what happened today?" to get the user to respond.
+ * - Based on the answer from the user, deduct focusPoints as indicated in README.md
  * - Go back to the AskInitialQuestion()
  */
-function CheckSleepProblems() {
-  readline.question('?QUESTION? (type "exit" to exit): ', (input) => {
+function CheckHomeworkProblems() {
+  readline.question('What happened today? (type "exit" to exit): ', (input) => {
     if (input.toLowerCase() === "exit") {
-      console.log("Thanks for tracking your rest. Sweet dreams!");
+      console.log("Thanks for tracking your study. Good luck!");
       readline.close();
       return;
     }
